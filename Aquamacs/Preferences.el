0e7f12bc2edfec379141a96419658549e4eddf78
Harri Feng <fenghaoran18@gmail.com>
2011-12-21 23:43:29 +0800

    add preference and customizations

---
+++ Aquamacs/Preferences.el
@@ -0,0 +1,4 @@
+(set-language-environment "Chinese-Big5")
+(prefer-coding-system 'utf-8)
+(setq mac-command-modifier 'meta)
+
---
+++ Aquamacs/customizations.el
@@ -0,0 +1,31 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 215 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(current-language-environment "Chinese-GBK")
+ '(default-frame-alist (quote ((internal-border-width . 0) (vertical-scroll-bars . right) (cursor-type . box) (alpha 85 80) (modeline . t) (fringe) (border-color . "#888a85") (mouse-color . "#ffdd33") (background-mode . dark) (tool-bar-lines . 1) (menu-bar-lines . 1) (right-fringe . 14) (left-fringe . 7) (cursor-color . "#ffdd33") (background-color . "#2e3436") (foreground-color . "#e4e4ef") (font . "-apple-YaHei_Consolas_Hybrid-medium-normal-normal-*-16-*-*-*-p-0-iso10646-1") (fontsize . 0) (font-backend ns))))
+ '(display-time-mode t)
+ '(ns-alternate-modifier nil)
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(size-indication-mode t)
+ '(visual-line-mode nil t))
+;(custom-set-faces
+  ;;; custom-set-faces was added by Custom.
+  ;;; If you edit it by hand, you could mess it up, so be careful.
+  ;;; Your init file should contain only one such instance.
+  ;;; If there is more than one, they won't work right.
+ ;'(org-mode-default ((t (:inherit outline-mode-default :stipple nil :background "textColor" :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "Monaco"))))
+ ;'(text-mode-default ((t (:inherit autoface-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 140 :width normal :family "YaHei Consolas Hybrid")))))
+ 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(autoface-default ((t (:inherit default))))
+ '(org-mode-default ((t (:inherit outline-mode-default :stipple nil :strike-through nil :underline nil :slant normal :weight normal :height 160 :width normal :family "YaHei Consolas Hybrid")))))
